39e4873dbebb67e66718408396e95d6831374289
JohnKossa <kg959ster@gmail.com>
2021-03-06 00:55:57 -0600

    Added a unidirectional send_game_chat function to DiscordConnection.py that sends messages outside the request-response pattern. Moved the game object out of the global namespace and included it as a property on the Discord Connection object

--- DiscordConnection.py
+++ DiscordConnection.py
@@ -17,16 +17,28 @@ intents.members = True
 class CustomClient(discord.Client):
     def __init__(self, intents=None):
         super().__init__(intents=intents)
+        self.game_channel = None
 
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
         print('Collecting users')
         guild = self.guilds[0]
         async for member in guild.fetch_members():
-            if UserUtils.get_user_by_username(str(member), game.discord_users) is None:
-                game.discord_users.append(DiscordUser(username=str(member)))
-        print('Users collected. Added {} users.'.format(len(game.discord_users)))
-        UserUtils.print_all(game.discord_users)
+            if UserUtils.get_user_by_username(str(member), self.game.discord_users) is None:
+                self.game.discord_users.append(DiscordUser(username=str(member)))
+        print('Users collected. Added {} users.'.format(len(self.game.discord_users)))
+        UserUtils.print_all(self.game.discord_users)
+
+        guild = self.guilds[0]
+        text_channels = guild.text_channels
+        print(text_channels)
+        self.game_channel = next(filter(lambda channel: channel.name == "muddle-game", text_channels), None)
+
+    async def send_game_chat(self, text, tagged_users=[]):
+        if self.game_channel is None:
+            print("No game channel set.")
+            return
+        await self.game_channel.send(text)
 
     async def on_message(self, message):
         user = self.user
@@ -38,12 +50,11 @@ class CustomClient(discord.Client):
             return
 
         # Collect user if not in users
-
-        discord_user = UserUtils.get_user_by_username(str(author), game.discord_users)
+        discord_user = UserUtils.get_user_by_username(str(author), self.game.discord_users)
         if discord_user is None:
             print("New user found. Adding...")
             discord_user = DiscordUser(username=str(author))
-            game.discord_user.append(discord_user)
+            self.game.discord_user.append(discord_user)
 
         if not content.startswith("!"):
             return
@@ -65,7 +76,7 @@ class CustomClient(discord.Client):
                 matched_command = possible_command
 
         if matched_command is not None:
-            await channel.send(matched_command.do_action(game, params, message))
+            await channel.send(matched_command.do_action(self.game, params, message))
             return
 
         if command.lower() == "get" and params[0].lower() == "ye" and params[1].lower() == "flask":
@@ -80,10 +91,8 @@ class CustomClient(discord.Client):
 
 
 client = CustomClient(intents=intents)
-game = None
 
 
 def run(game_ref):
-    global game
-    game = game_ref
+    client.game = game_ref
     client.run(TOKEN)
